bd46538176fb9f41f724bad8d110dce8db42231e
Kevin Delgado <kevindel891@gmail.com>
2023-11-07 18:15:46 -0800

    Create simple timer

--- RubikApp/pages/Timer.js
+++ RubikApp/pages/Timer.js
@@ -1,9 +1,35 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Button } from 'react-native';
+import * as React from 'react';
 
-export default PastAttempt = () => {
+
+export default Timer = () => {
+
+  const [counter, setCounter] = React.useState(0);
+  const [second, setSecond] = React.useState(0);
+  const [start, setStart] = React.useState(false);
+
+
+
+  React.useEffect(() => {
+    if(start){
+  
+      //Issue, Timer is slow due to SetTimeout has a min delay of 4 milliseconds 
+      //Band-aid solution, set counter value to increased number
+      
+      if(counter < 99){
+        setTimeout(() => setCounter(counter + 1.666666), 1);
+
+      }else{
+        setCounter(0);
+        setSecond(second + 1);
+      }
+    }
+  }, [counter, start]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> {second} : {Math.round(counter)}</Text>
+      <Button title='Start' onPress={() => setStart(!start)}/>
       <StatusBar style="auto" />
     </View>
   );
