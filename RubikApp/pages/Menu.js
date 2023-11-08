7ac3d0c5e7907ff868518ac5962085fcf2f64534
Kevin Delgado <kevindel891@gmail.com>
2023-11-07 18:28:45 -0800

    Change menu layout

--- RubikApp/pages/Menu.js
+++ RubikApp/pages/Menu.js
@@ -3,9 +3,16 @@ import { StyleSheet, Text, View, StatusBar, Button } from 'react-native';
 export default Menu = ({navigation}) => {
   return (
     <View style={styles.container}>
+      <View style= {{flex: 1, justifyContent:"center"}}>
+      <Text style={{fontSize:42}}>Rubiks Helper</Text>
+      </View>
+
+      <View style={{flex: 1, justifyContent: 'space-evenly'}}>
       <Button title='Solve Cube' onPress={() => navigation.navigate("RubikSolver")}/>
       <Button title='Timer'onPress={() => navigation.navigate("Timer")}/>
       <Button title='LeaderBoard'onPress={() => navigation.navigate("PastAttempts")}/>
+      <Button title='User Stats'/>
+      </View>
     </View>
   );
 }
