0cf922b5efdf28cbf6b1dd566f72ae354f6325f4
Kevin Delgado <kevindel891@gmail.com>
2023-11-07 19:11:37 -0800

    Add additional Buttons

--- RubikApp/pages/Timer.js
+++ RubikApp/pages/Timer.js
@@ -1,25 +1,19 @@
-import { StyleSheet, Text, View, StatusBar, Button } from 'react-native';
+import {StyleSheet, Text, View, StatusBar, Button} from 'react-native';
 import * as React from 'react';
 
-
 export default Timer = () => {
-
   const [counter, setCounter] = React.useState(0);
   const [second, setSecond] = React.useState(0);
   const [start, setStart] = React.useState(false);
 
-
-
   React.useEffect(() => {
-    if(start){
-  
-      //Issue, Timer is slow due to SetTimeout has a min delay of 4 milliseconds 
-      //Band-aid solution, set counter value to increased number
-      
-      if(counter < 99){
-        setTimeout(() => setCounter(counter + 1.666666), 1);
+    if (start) {
+      //Issue: Timer is slow due to SetTimeout has a min delay of 4 milliseconds
+      //Band-aid solution : set counter value to increased number
 
-      }else{
+      if (counter < 99) {
+        setTimeout(() => setCounter(counter + 1.66666666666), 1);
+      } else {
         setCounter(0);
         setSecond(second + 1);
       }
@@ -28,12 +22,33 @@ export default Timer = () => {
 
   return (
     <View style={styles.container}>
-      <Text> {second} : {Math.round(counter)}</Text>
-      <Button title='Start' onPress={() => setStart(!start)}/>
-      <StatusBar style="auto" />
+      <View>
+        <Text>
+          {' '}
+          {second < 10 ? '0' : null}
+          {second} : {Math.round(counter) < 10 ? '0' : null}
+          {Math.round(counter)}
+        </Text>
+      </View>
+      <View>
+        <Button
+          title={!start ? 'Start' : 'Stop'}
+          onPress={() => setStart(!start)}
+        />
+        //Issue: Save and Reset buttons disabled until 1 second has passed
+        <Button
+          title="Save"
+          disabled={counter < 0 ? true : second === 0 ? true : false}
+        />
+        <Button
+          title="Reset"
+          disabled={counter < 0 ? true : second === 0 ? true : false}
+        />
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
