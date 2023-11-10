22c7a8cc979848e5e40124ac4052f6503b57c445
Kevin Delgado <kevindel891@gmail.com>
2023-11-09 18:46:26 -0800

    Update Local Stats formatting

--- RubikApp/pages/StatsLocal.js
+++ RubikApp/pages/StatsLocal.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, StatusBar, FlatList } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, FlatList   } from 'react-native';
 import AppContext from '../AppContext';
 import * as React from 'react';
 
@@ -8,20 +8,13 @@ export default StatsLocal = () => {
 
   return (
     <View style={styles.container}>
-      <View style= {{width: "80%"}}>
+      <View style= {styles.list}>
         <FlatList
           data={context.leaderBoard}
           renderItem={({item}) => {
             if(item.user === context.curUser){
               return(
-                <View style={{flex: 1,
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
-                  borderColor: 'grey',
-                  borderWidth: 1,
-                  marginVertical: 10,
-                  padding: 10}}>
+                <View style={styles.listItem}>
                 <Text>{item.user}</Text>
                 <Text>{item.finalTime}</Text>
               </View>
@@ -45,4 +38,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  listItem:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderColor: 'grey',
+    borderWidth: 1,
+    marginVertical: 10,
+    padding: 10
+  },
+  list:{
+    width: "80%"
+  }
 });
