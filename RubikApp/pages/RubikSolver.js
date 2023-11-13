2b4fb1b2ac77c1bb57acbd5371d8770012e35c11
Kevin Delgado <kevindel891@gmail.com>
2023-11-12 17:36:18 -0800

    Add simple image usage and display

--- RubikApp/pages/RubikSolver.js
+++ RubikApp/pages/RubikSolver.js
@@ -1,9 +1,25 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-
+import { StyleSheet, Text, View, StatusBar, Image, Button } from 'react-native';
+import ImageCropPicker from 'react-native-image-crop-picker';
+import * as React from 'react';
 export default PastAttempt = () => {
+
+  const [saveImage, setSaveImage] = React.useState('');
+
+  const takeImage = () =>{
+    ImageCropPicker.openCamera({
+      width: 300,
+      height: 300,
+      cropping: true,
+    }).then(image => {
+      console.log(image.path)
+      setSaveImage(image.path)
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title='Take Image' onPress={() => {takeImage()}}/>
+      <Image source={{ uri: saveImage }} style={{ width: 300, height: 300 }} />
       <StatusBar style="auto" />
     </View>
   );
