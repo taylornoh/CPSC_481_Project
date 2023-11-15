c325dfc3f09a812962eeede53795f8ff4ca18d60
Kevin Delgado <kevindel891@gmail.com>
2023-11-14 18:52:35 -0800

    Create basic backend for image reception

---
+++ BackEndApi/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, Response, request, jsonify
+from io import BytesIO
+import base64
+from flask_cors import CORS, cross_origin
+import os
+import sys
+
+app = Flask(__name__)
+cors = CORS(app)
+
+def imageNaming():
+    file_path= 'images/image0.jpg'
+    
+    if(checkFullImage()):
+        clearPhotos()
+
+    for num in range(6):
+        if(os.path.isfile(file_path)):
+            file_path = "images/image" + str(num) + ".jpg"
+        else:
+            break
+
+    return file_path
+
+
+def checkFullImage():
+    if(os.path.isfile("images/image5.jpg")):
+        return True
+    return False
+
+
+def clearPhotos():
+    for num in range(6):
+        os.remove("images/image" + str(num) + ".jpg")
+
+
+@app.route("/image", methods=['GET', 'POST'])
+def image():
+    if(request.method == "POST"):
+        bytesOfImage = request.get_data()
+        file_path = imageNaming()
+        with open(file_path, 'wb') as out:
+            out.write(bytesOfImage)
+        return "Image read"
