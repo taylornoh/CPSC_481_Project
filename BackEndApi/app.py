4bccac2aeb0235fde3e15bd5242590353c222021
Kevin Delgado <kevindel891@gmail.com>
2023-11-20 15:50:37 -0800

    Add function to find image colors and return to front end

--- BackEndApi/app.py
+++ BackEndApi/app.py
@@ -4,9 +4,19 @@ import base64
 from flask_cors import CORS, cross_origin
 import os
 import sys
+import cv2
+import numpy as np
+
 
 app = Flask(__name__)
 cors = CORS(app)
+
+facecolors = {"front": [],
+                "top": [],
+                "back": [],
+                "bottom": [],
+                "left": [],
+                "right": []}
 
 def imageNaming():
     file_path= 'images/image0.jpg'
@@ -33,6 +43,43 @@ def clearPhotos():
     for num in range(6):
         os.remove("images/image" + str(num) + ".jpg")
 
+def getColors():
+    colorBounds = {"red" : [np.array([39, 14, 0], dtype="uint8"), np.array([95, 120, 255], dtype="uint8")],
+                "yellow ": [np.array([10, 127, 130], dtype="uint8"), np.array([30, 230, 230], dtype="uint8")],
+                "orange": [np.array([4, 140, 173], dtype="uint8"), np.array([80, 180, 250],dtype="uint8")],
+                "blue" : [np.array([121, 118, 0], dtype='uint8'), np.array([255, 255, 100],  dtype='uint8')],
+                "green" : [np.array([3, 0, 20], dtype='uint8'), np.array([26, 255, 120], dtype='uint8')],
+                "white" : [np.array([120, 120, 160], dtype='uint8'), np.array([255, 255, 255], dtype='uint8')]
+                }
+
+    for picture in range(6):
+        image = cv2.imread('images/image' + str(picture) + '.jpg')
+        for rowStart in range(0, 300, 100):
+            rowEnd = rowStart + 100
+            for colStart in range(0, 300, 100):
+                colorFound = False
+                colEnd = colStart + 100
+                #Get Center of cube face
+                (cX, cY) = (colEnd, rowEnd)
+                #Each Rubik Cube Piece will be 100X100 pixels 
+
+                #slice image to only get  middle
+                t1 = image[rowStart:cY, colStart:cX]
+                (h,w) = t1.shape[:2]
+                t2 = t1[25:75, 25:75]
+                for colors in colorBounds:            
+                    lower = colorBounds[colors][0]
+                    upper = colorBounds[colors][1]
+
+                    mask = cv2.inRange(t2, lower, upper)
+                    detected_output = cv2.bitwise_and(t2, t2, mask=mask)
+                    found = cv2.countNonZero(mask)
+                    firstKey = list(facecolors)[picture]
+                    if(found > 60):
+                        facecolors[firstKey].append(colors)
+                        break
+                    if(colors == "white"):
+                        facecolors[firstKey].append("black")
 
 @app.route("/image", methods=['GET', 'POST'])
 def image():
@@ -43,5 +90,6 @@ def image():
             out.write(bytesOfImage)
 
         if(checkFullImage()):
-            return "All images Read"
+            getColors()
+            return facecolors
         return "Image read"
