a9f045f40173e75a79817f6146ae68ce808d657a
Taylor <taylornoh@csu.fullerton.edu>
2023-11-30 20:39:57 -0800

    Update app.py

--- BackEndApi/app.py
+++ BackEndApi/app.py
@@ -5,12 +5,8 @@ from flask_cors import CORS, cross_origin
 import os
 import sys
 import json
-from cube import RubiksCube
-from solver import IDA_star, build_heuristic_db
 
-MAX_MOVES = 5
-NEW_HEURISTICS = False
-HEURISTIC_FILE = 'heuristic.json'
+from twophase import solve
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -56,21 +52,49 @@ def image():
 
 
 def construct_cube_state_str(cube_state_dict):
-    faces = ["Top", "Left", "Front", "Right", "Back", "Bottom"]
+    faces = ["Up", "Right", "Front", "Down", "Left", "Back"]
     cube_state_str = ""
+
+    face_color_mapping = {}
+    for face in faces:
+        middle_color = cube_state_dict[face][4]
+        face_color_mapping[middle_color[0]] = face[0]
 
     # Iterating through the faces in the standard Rubix cube layout order
     for face in faces:
-        if face == "Top":
-            face = "Up"
-        elif face == "Bottom":
-            face = "Down"
         # Grabbing the first letter from each square and inserting it into the string
         for square in cube_state_dict[face]:
-            cube_state_str += square[0]
-    
+            cube_state_str += face_color_mapping[square[0]]
+
     return cube_state_str
 
+def construct_detailed_steps(steps):
+    detailed_steps = []
+    move_mapping = {
+        "F": "90 degree clockwise turn of Front face",
+        "R": "90 degree clockwise turn of Right face",
+        "U": "90 degree clockwise turn of Upper face",
+        "L": "90 degree clockwise turn of Left face",
+        "B": "90 degree clockwise turn of Back face",
+        "D": "90 degree clockwise turn of Down face",
+        "F'": "90 degree counterclockwise turn of Front face",
+        "R'": "90 degree counterclockwise turn of Right face",
+        "U'": "90 degree counterclockwise turn of Upper face",
+        "L'": "90 degree counterclockwise turn of Left face",
+        "B'": "90 degree counterclockwise turn of Back face",
+        "D'": "90 degree counterclockwise turn of Down face",
+        "F2": "180 degree turn of Front face",
+        "R2": "180 degree turn of Right face",
+        "U2": "180 degree turn of Upper face",
+        "L2": "180 degree turn of Left face",
+        "B2": "180 degree turn of Back face",
+        "D2": "180 degree turn of Down face"
+    }
+
+    for step in steps:
+        detailed_steps.append(move_mapping[step])
+    
+    return detailed_steps
 
 @app.route("/steps", methods=['GET', 'POST'])
 def steps():
@@ -78,46 +102,13 @@ def steps():
         with open("test.json", "r") as json_cube:
             cube_state_dict = json.load(json_cube)
             cube_state_str = construct_cube_state_str(cube_state_dict)
-
-            cube = RubiksCube(state=cube_state_str)
-            cube.show()
-
-            if os.path.exists(HEURISTIC_FILE):
-                with open(HEURISTIC_FILE) as f:
-                    h_db = json.load(f)
-            else:
-                h_db = None
-
-            if h_db is None or NEW_HEURISTICS is True:
-                actions = [(r, n, d) for r in ['h', 'v', 's'] for d in [0, 1] for n in range(cube.n)]
-                h_db = build_heuristic_db(
-                    cube.stringify(),
-                    actions,
-                    max_moves = MAX_MOVES,
-                    heuristic = h_db
-                )
-
-                with open(HEURISTIC_FILE, 'w', encoding='utf-8') as f:
-                    json.dump(
-                        h_db,
-                        f,
-                        ensure_ascii=False,
-                        indent=4
-                    )
             
-            cube.shuffle(l_rot = MAX_MOVES if MAX_MOVES < 5 else 5, u_rot = MAX_MOVES)
-            #--------------------------------
-            solver = IDA_star(h_db)
-            moves = solver.run(cube.stringify())
-            print(moves)
+            solution = {
+                "steps": [],
+                "detailed_steps": []
+            }
 
-            for m in moves:
-                if m[0] == 'h':
-                    cube.horizontal_twist(m[1], m[2])
-                elif m[0] == 'v':
-                    cube.vertical_twist(m[1], m[2])
-                elif m[0] == 's':
-                    cube.side_twist(m[1], m[2])
-            cube.show()
+            solution["steps"] = solve(cube_state_str).split()
+            solution["detailed_steps"] = construct_detailed_steps(solution["steps"])
 
-            return moves
+            return jsonify(solution)
